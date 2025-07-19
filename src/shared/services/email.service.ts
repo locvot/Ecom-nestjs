66ac90823d7ed44f1fa5851a769b6dcb80cd841e
locvot <vol6921@gmail.com>
2025-07-19 20:51:37 +0700

    send email using template html

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from '../config'
+import fs from 'fs'
+import path from 'path'
+
+const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), {
+  encoding: 'utf-8',
+})
 
 @Injectable()
 export class EmailService {
@@ -10,11 +16,12 @@ export class EmailService {
   }
 
   sendOTP(payload: { email: string; code: string }) {
+    const subject = 'OTP code'
     return this.resend.emails.send({
       from: 'Ecom <onboarding@resend.dev>',
       to: [payload.email],
-      subject: 'OTP code',
-      html: `<strong>${payload.code}</strong>`,
+      subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
