9dffe37b308dcb23fc09664f050d7a5868c12015
locvot <vol6921@gmail.com>
2025-07-24 15:27:46 +0700

    add deleted

--- src/shared/models/shared-permission.model.ts
+++ src/shared/models/shared-permission.model.ts
@@ -17,6 +17,8 @@ export const PermissionSchema = z.object({
   ]),
   createdById: z.number().nullable(),
   updatedById: z.number().nullable(),
+  deletedById: z.number().nullable(),
+  deletedAt: z.date().nullable(),
   createdAt: z.date(),
   updatedAt: z.date(),
 })
--- src/shared/models/shared-role.model.ts
+++ src/shared/models/shared-role.model.ts
@@ -8,6 +8,7 @@ export const RoleSchema = z.object({
   createdById: z.number().nullable(),
   updatedById: z.number().nullable(),
   deletedById: z.number().nullable(),
+  deletedAt: z.date().nullable(),
   createdAt: z.date(),
   updatedAt: z.date(),
 })
