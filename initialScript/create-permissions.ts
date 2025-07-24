ad393ecdd4f6448ae51b5b0342e8ae135548d929
locvot <vol6921@gmail.com>
2025-07-24 17:09:35 +0700

    check role permission when user request

--- initialScript/create-permissions.ts
+++ initialScript/create-permissions.ts
@@ -30,13 +30,13 @@ async function bootstrap() {
     })
     .filter((item) => item !== undefined)
 
-  // Tạo object permissionInDbMap với key là [method-path]
+  // Create object permissionInDbMap with key [method-path]
   const permissionInDbMap: Record<string, (typeof permissionsInDb)[0]> = permissionsInDb.reduce((acc, item) => {
     acc[`${item.method}-${item.path}`] = item
     return acc
   }, {})
 
-  // Tạo object availableRoutesMap với key là [method-path]
+  // Create object availableRoutesMap with key [method-path]
   const availableRoutesMap: Record<string, (typeof availableRoutes)[0]> = availableRoutes.reduce((acc, item) => {
     acc[`${item.method}-${item.path}`] = item
     return acc
@@ -46,7 +46,7 @@ async function bootstrap() {
     return !availableRoutesMap[`${item.method}-${item.path}`]
   })
 
-  // Xóa permissions không tồn tại trong availableRoutes
+  // Delete permissions not exist in availableRoutes
   if (permissionsToDelete.length > 0) {
     const deleteResult = await prisma.permission.deleteMany({
       where: {
@@ -60,12 +60,12 @@ async function bootstrap() {
     console.log('No permissions to delete')
   }
 
-  // Tìm routes mà không tồn tại trong permissionsInDb
+  //Find routes which not exist in permissionsInDb
   const routesToAdd = availableRoutes.filter((item) => {
     return !permissionInDbMap[`${item.method}-${item.path}`]
   })
 
-  // Thêm các routes này dưới dạng permissions database
+  // Add routes as permissions database
   if (routesToAdd.length > 0) {
     const permissionsToAdd = await prisma.permission.createMany({
       data: routesToAdd,
@@ -82,7 +82,7 @@ async function bootstrap() {
       deletedAt: null,
     },
   })
-  // Cập nhật lại các permissions trong Admin Role
+  // Update other permissions in Admin Role
   const adminRole = await prisma.role.findFirstOrThrow({
     where: {
       name: RoleName.Admin,
