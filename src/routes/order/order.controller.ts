7aa18224b3436d0f2728702f8295d038fe3f1ab0
locvot <vol6921@gmail.com>
2025-08-05 20:16:18 +0700

    create order api

--- src/routes/order/order.controller.ts
+++ src/routes/order/order.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query } from '@nestjs/common'
 import { OrderService } from './order.service'
 import { ZodSerializerDto } from 'nestjs-zod'
-import { GetOrderListResDTO } from './order.dto'
+import { CreateOrderBodyDTO, CreateOrderResDTO, GetOrderListQueryDTO, GetOrderListResDTO } from './order.dto'
 import { ActiveUser } from 'src/shared/decorators/active-user.decorator'
 
 @Controller('order')
@@ -10,7 +10,13 @@ export class OrderController {
 
   @Get()
   @ZodSerializerDto(GetOrderListResDTO)
-  getCart(@ActiveUser('userId') userId: number, @Query() query: GetOrderListResDTO) {
+  getCart(@ActiveUser('userId') userId: number, @Query() query: GetOrderListQueryDTO) {
     return this.orderService.list(userId, query)
+  }
+
+  @Post()
+  @ZodSerializerDto(CreateOrderResDTO)
+  create(@ActiveUser('userId') userId: number, @Body() body: CreateOrderBodyDTO) {
+    return this.orderService.create(userId, body)
   }
 }
--- src/routes/order/order.repo.ts
+++ src/routes/order/order.repo.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/shared/services/prisma.service'
-import { GetOrderListQueryType, GetOrderListResType } from './order.model'
+import { CreateOrderBodyType, CreateOrderResType, GetOrderListQueryType, GetOrderListResType } from './order.model'
 import { Prisma } from '@prisma/client'
+import {
+  NotFoundCartItemException,
+  OutOfStockSKUException,
+  ProductNotFoundException,
+  SKUNotBelongToShopException,
+} from './order.error'
+import { OrderStatus } from 'src/shared/constants/order.constant'
 
 @Injectable()
 export class OrderRepo {
@@ -41,6 +48,125 @@ export class OrderRepo {
       limit,
       totalItems,
       totalPages: Math.ceil(totalItems / limit),
+    }
+  }
+
+  async create(userId: number, body: CreateOrderBodyType) {
+    // Check carItemIds in db
+    const allBodyCartItemIds = body.map((item) => item.cartItemIds).flat()
+    const cartItems = await this.prismaService.cartItem.findMany({
+      where: {
+        id: {
+          in: allBodyCartItemIds,
+        },
+        userId,
+      },
+      include: {
+        sku: {
+          include: {
+            product: {
+              include: { productTranslations: true },
+            },
+          },
+        },
+      },
+    })
+
+    if (cartItems.length !== allBodyCartItemIds.length) {
+      throw NotFoundCartItemException
+    }
+
+    // Check if quantity > stock
+    const isOutOfStock = cartItems.some((item) => {
+      return item.sku.stock < item.quantity
+    })
+    if (isOutOfStock) {
+      throw OutOfStockSKUException
+    }
+
+    // Check if any cart item is deleted
+    const isExistNotReadyProduct = cartItems.some(
+      (item) =>
+        item.sku.product.deletedAt !== null ||
+        item.sku.product.publishedAt === null ||
+        item.sku.product.publishedAt > new Date(),
+    )
+    if (isExistNotReadyProduct) {
+      throw ProductNotFoundException
+    }
+
+    // Check skuID in cartItem and shopId is valid
+    const cartItemMap = new Map<number, (typeof cartItems)[0]>()
+    cartItems.forEach((item) => {
+      cartItemMap.set(item.id, item)
+    })
+    const isValidShop = body.every((item) => {
+      const bodyCartItemIds = item.cartItemIds
+      return bodyCartItemIds.every((cartItemId) => {
+        const cartItem = cartItemMap.get(cartItemId)
+        return item.shopId === cartItem?.sku.createdById
+      })
+    })
+    if (!isValidShop) {
+      throw SKUNotBelongToShopException
+    }
+
+    // Create order and delete cart item
+    const orders = await this.prismaService.$transaction(async (tx) => {
+      const orders = await Promise.all(
+        body.map((item) =>
+          tx.order.create({
+            data: {
+              userId,
+              status: OrderStatus.PENDING_PAYMENT,
+              receiver: item.receiver,
+              createdById: userId,
+              shopId: item.shopId,
+              items: {
+                create: item.cartItemIds.map((cartItemId) => {
+                  const cartItem = cartItemMap.get(cartItemId)!
+                  return {
+                    productName: cartItem.sku.product.name,
+                    skuPrice: cartItem.sku.price,
+                    image: cartItem.sku.image,
+                    skuId: cartItem.sku.id,
+                    skuValue: cartItem.sku.value,
+                    quantity: cartItem.quantity,
+                    productId: cartItem.sku.product.id,
+                    productTranslations: cartItem.sku.product.productTranslations.map((translation) => {
+                      return {
+                        id: translation.id,
+                        name: translation.name,
+                        description: translation.description,
+                        languageId: translation.languageId,
+                      }
+                    }),
+                  }
+                }),
+              },
+              products: {
+                connect: item.cartItemIds.map((cartItemId) => {
+                  const cartItem = cartItemMap.get(cartItemId)!
+                  return {
+                    id: cartItem.sku.product.id,
+                  }
+                }),
+              },
+            },
+          }),
+        ),
+      )
+      await tx.cartItem.deleteMany({
+        where: {
+          id: {
+            in: allBodyCartItemIds,
+          },
+        },
+      })
+      return orders
+    })
+    return {
+      data: orders,
     }
   }
 }
