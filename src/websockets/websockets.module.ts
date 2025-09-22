b67b2a8df79bf559196375b42b0d28fdf25d648b
locvot <vol6921@gmail.com>
2025-09-23 04:13:08 +0700

    remove websockets module

--- src/websockets/websockets.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ChatGateway } from './chat.gateway'
-import { PaymentGateway } from './payment.gateway'
-
-@Module({
-  providers: [ChatGateway, PaymentGateway],
-})
-export class WebsocketsModule {}
